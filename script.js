250ab132244c850b5d40f7466976e667ec7c5180
Emmanuel <gramme.oe@gmail.com>
2019-06-07 03:52:15 +0100

    Exposed two functions as my API

--- script.js
+++ script.js
@@ -24,29 +24,33 @@ var app = (function() {
 		$template = $el.find("#contactTemplate").html();
 
 	//bind events
-	$input.on("keypress", preSave);
-	$button.on("click", preSave);
+	$input.on("keypress", _preSave);
+	$button.on("click", _preSave);
 	$container.on("click", "span.delete", deletePerson);
 
-	//render
-	function render() {
+	//_render
+	function _render() {
 		var data = { card: contact };
 		$container.html(Mustache.render($template, data));
 	}
 
 	//init
-	render();
+	_render();
 
 	//functionalities
-	function preSave(event) {
+	function _preSave(event) {
 		if ($nameForm.val() != "" && $numberForm.val() != "") {
 			if (event.which == 13 || event.type == "click") addPerson();
 		}
 	}
 
-	function addPerson() {
-		let nameInp = $nameForm.val(),
-			numberInp = parseInt($numberForm.val());
+	function addPerson(value) {
+		if (typeof value === "object") {
+			(nameInp = value.name), (numberInp = value.number);
+		} else {
+			(nameInp = $nameForm.val()),
+				(numberInp = parseInt($numberForm.val()));
+		}
 
 		if (isNaN(numberInp)) {
 			alert("Enter a valid phone number");
@@ -54,20 +58,32 @@ var app = (function() {
 		} else {
 			let entry = { name: nameInp, number: numberInp };
 			contact.push(entry);
-			render();
+			_render();
 			$input.val("");
 		}
 	}
 
 	function deletePerson(event) {
-		var $remove = $(event.target).closest("div.wrapper"),
+		let i;
+		if (typeof event === "number") {
+			i = event - 1;
+		} else {
+			var $remove = $(event.target).closest("div.wrapper");
 			i = $container.find(".wrapper").index($remove);
+		}
 		contact.splice(i, 1);
-		render();
+		_render();
 	}
 
-	// return {
-	// 	addPerson: addPerson,
-	// 	deletePerson: deletePerson
-	// };
+	console.log(`TO ADD CONTACT
+	Create an object with two properties "name" and "number", then call app.addPerson(<name of object>)
+
+
+	TO DELETE CONTACT
+	Run app.deletePerson(<serial number of the contact>) eg app.deletePerson(2)`);
+
+	return {
+		addPerson: addPerson,
+		deletePerson: deletePerson
+	};
 })();
